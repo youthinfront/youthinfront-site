35fbadbe2cb7a4760c4d63fcc781cbca6ce56bb7
Indranil Bora <indranil.bora@gmail.com>
2018-03-07 03:13:03 -0500

    Modal JS

---
+++ js/modal.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('[data-popup-open]').on('click', function (e) {
+        const targeted_popup_class = jQuery(this).attr('data-popup-open');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+        e.preventDefault();
+    });
+    $('[data-popup-close]').on('click', function (e) {
+        const targeted_popup_class = jQuery(this).attr('data-popup-close');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+        e.preventDefault();
+    });
+});
