70737beb5815c67bdb9d4aaf3ed3c1b5bd2a6324
Marco Morales <marco@edx.org>
2018-03-07 03:28:37 -0500

    fixup! revert video work temporarilty

--- js/tweet.js
+++ js/tweet.js
@@ -38,7 +38,9 @@
         var height = width*5625
 
         var url = "https://www.youtube.com/embed/" + id + "?rel=0";
-        element.innerHTML = '<iframe width="' + width + '" height="' + height + '"' + 'src="' + url + '" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+        //element.innerHTML = '<iframe width="' + width + '" height="' + height + '"' + 'src="' + url + '" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+        element.innerHTML = '<iframe width="560px height="290" ' + 'src="' + url + '" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+    }
     }
 
     function extractYoutubeVideoId(link) {
