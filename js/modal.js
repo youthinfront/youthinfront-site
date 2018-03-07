25b3fa8ab80c997651981b8fef6a99f07aca0637
Indranil Bora <indranil.bora@gmail.com>
2018-03-07 09:31:12 -0500

    Fix modal slowness

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,6 @@
 $(function () {
     $('[data-popup-open]').on('click', function (e) {
         const targeted_popup_class = $(this).attr('data-popup-open');
-        $('[data-popup="' + targeted_popup_class + '"]').appendTo('main');
         $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
         e.preventDefault();
     });
