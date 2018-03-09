0142b65a164a9ac8604e08709c206516a10655b8
Indranil Bora <indranil.bora@gmail.com>
2018-03-08 21:22:10 -0800

    Commit

--- js/modal.js
+++ js/modal.js
@@ -2,7 +2,7 @@ $(function () {
     $('[data-popup-open]').on('click', function (e) {
         const targeted_popup_class = $(this).attr('data-popup-open');
         const targeted_popup_name = $(this).attr('data-popup-name');
-        document.getElementById(targeted_popup_class).src=`https://www.youtube.com/embed/${targeted_popup_class}?rel=0&autoplay=1`;
+        document.getElementById(targeted_popup_class).src = `https://www.youtube.com/embed/${targeted_popup_class}?rel=0&autoplay=1`;
         $('[data-popup="' + targeted_popup_class + '"]').appendTo('main');
         $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
         e.preventDefault();
@@ -14,7 +14,7 @@ $(function () {
     });
     $('[data-popup-close]').on('click', function (e) {
         const targeted_popup_class = $(this).attr('data-popup-close');
-        document.getElementById(targeted_popup_class).src='';
+        document.getElementById(targeted_popup_class).src = '';
         $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
         e.preventDefault();
         if (window.gtag) {
